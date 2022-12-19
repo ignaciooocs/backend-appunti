95a3814fb6d479b75dadb7f7a99d8a61c26993da
ignacioocode <naxomc25@gmail.com>
2022-12-19 17:02:42 -0300

    new change

--- mongo.js
+++ mongo.js
@@ -2,20 +2,9 @@ const mongoose = require('mongoose')
 
 const connectionString = process.env.DATA_BASE_URI
 
-const connect = async () => {
-  try {
-    await mongoose.connect(connectionString)
-    console.log('dataBase Connected')
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-connect()
-
-// mongoose.connect(connectionString)
-//   .then(() => {
-//     console.log('dataBase connected')
-//   }).catch(() => {
-//     console.log('ocurrio un error')
-//   })
+mongoose.connect(connectionString)
+  .then(() => {
+    console.log('dataBase connected')
+  }).catch(() => {
+    console.log('ocurrio un error')
+  })
